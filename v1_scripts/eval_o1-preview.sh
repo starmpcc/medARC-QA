05035164e2787ff69e92a53941aae0c6f9d62219
dbernardo05 <43284380+dbernardo05@users.noreply.github.com>
2025-01-26 06:39:47 -0800

    Delete v1_scripts/eval_o1-preview.sh

--- v1_scripts/eval_o1-preview.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ../
-
-python evaluate_from_api.py \
-                 --model_name o1-preview\
-                 --output_dir eval_results \
-                 --assigned_subjects all
